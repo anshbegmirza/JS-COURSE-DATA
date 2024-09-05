9b7af5065ab6607bb386c384fe253d199263a53d
anshbegmirza <anshbegmirza@gmail.com>
2024-09-05 16:12:32 +0530

    Day 4 of JS

--- 3/script.js
+++ 3/script.js
@@ -232,6 +232,7 @@ console.log(ages);
 // 040 Basic Array Methods
 //**********************
 
+/*
 // Add elements
 const friends = ['Michael', 'Steven', 'Peter'];
 friends.push('jay', 'stevie', 'bob', 'maverick'); // adds elements to array at the end
@@ -260,5 +261,349 @@ console.log(friends.includes('BOB'));
 
 if (friends.includes('Peter')) {
   console.log(`You have a friend called Peter`);
+
+}
+*/
+
+//********************** 
+// 041 Array Challende #1
+//**********************
+
+/*
+// my solution
+
+// function calcTip(bill) {
+//   if (bill >= 50 && bill <= 300) {
+//     return 0.15 * bill;
+//   }
+//   else {
+//     return 0.20 * bill;
+//   }
+// }
+// let bill1 = calcTip(125) + 125;
+// let bill2 = calcTip(555) + 555;
+// let bill3 = calcTip(44) + 44;
+// console.log(bill1, bill2, bill3);
+
+
+// const total = [bill1, bill2, bill3];
+
+// console.log(total);
+
+// const trial = [calcTip(125) + 125, calcTip(555) + 555, calcTip(44) + 44,];
+// console.log(trial);
+
+
+//jonas solution
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(total);
+console.log(`Total for bill ${bills[0]} is ${total[0]} with tip ${tips[0]}.`);
+console.log(`Total for bill ${bills[1]} is ${total[1]} with tip ${tips[1]}.`);
+console.log(`Total for bill ${bills[2]} is ${total[2]} with tip ${tips[2]}.`);
+
+*/
+
+
+//********************** 
+// 042 Introduction to Objects
+//**********************
+
+/*
+const JonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  '2037-1991',
+  'Teacher',
+  ['Micheal', 'Peter', 'Jack', 'Rose']
+];
+
+console.log(JonasArray);
+//curly brackets for object
+const jonas = {
+  firstName: 'Jonas',
+  lastname: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Micheal', 'Peter', 'Jack', 'Rose']
+};
+*/
+
+//********************** 
+// 043 Dot vs Bracket Notation
+//**********************
+
+/*
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Micheal', 'Peter', 'Jack', 'Rose']
+};
+
+console.log(jonas);
+console.log(jonas.friends[3]);
+console.log(jonas.lastname);
+console.log(jonas['lastname']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, friends');
+
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonas';
+// console.log(jonas);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log(`Error 404 !`);
+}
+
+// challenge
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`);
+
+*/
+
+//********************** 
+// 044 Objects Methods
+//**********************
+
+
+/*
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 1991,
+  hasDriverLicense: true,
+  job: 'teacher',
+  friends: ['Micheal', 'Peter', 'Jack', 'Rose'],
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+
+  // calcAge: function () {
+
+  //   // console.log(this);//logs whole jonas object
+
+  //   return 2037 - this.birthYear;
+
+  //   // return 2037 - jonas.birthYear; //not ideal
+  // }
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriverLicense ? 'a' : 'no'} driver's license.`;
+  }
+
+};
+
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+
+
+// console.log(jonas['calcAge'](1991));
+
+//keep your code dry : DO NOT REPEAT YOURSELF
+
+// challenge
+
+// console.dir(jonas.getSummary());
+
+console.log(jonas.getSummary());
+
+*/
+
+
+//********************** 
+// 045 Coding Challenge #3
+//**********************
+
+// Objects challenge
+
+/*
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+}
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+}
+mark.calcBMI();
+john.calcBMI();
+
+
+// const display = mark.calcBMI() > john.calcBMI() ? `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s BMI (${john.calcBMI()})` : `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s bmi (${mark.calcBMI()})`;
+// console.log(display);
+
+console.log(mark.bmi, john.bmi);
+
+
+const display = mark.bmi > john.bmi ? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})` : `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s bmi (${mark.bmi})`;
+console.log(display);
+
+*/
+
+//********************** 
+// 046 FOR LOOP
+//**********************
+
+/*
+//counter loop
+//for loop keeps running while condition is TRUE
+for (let i = 1; i <= 10; i++) {
+  console.log(`Repetition ${i}`);
+}
+
+*/
+
+
+//********************** 
+// 047 Looping Arrays Breaking and Continuing
+//**********************
+
+
+/*
+// Really nice lecture to revise arrays.
+
+const JonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'Teacher',
+  ['Micheal', 'Peter', 'Jack', 'Rose'],
+
+];
+
+// always try to make your code dynamic and not hardcore.
+const types = [];
+for (let i = 0; i <= JonasArray.length - 1; i++) {
+
+  //reading from JonasArray
+  console.log(`${i + 1} element of array is ${JonasArray[i]}`, typeof JonasArray[i]);
+
+  //filling a array
+  // types[i] = typeof JonasArray[i];
+  types.push(typeof JonasArray[i]);
+}
+
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+for (let i = 0; i <= years.length - 1; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+
+//continue and break
+
+// continue is use to excess the current iteration of the loop.
+console.log(`--- ONLY STRINGS ALLOWED ---`);
+
+for (let i = 0; i < JonasArray.length; i++) {
+  if (typeof JonasArray[i] !== 'string') continue;
+  console.log(JonasArray[i], typeof JonasArray[i]);
+}
+
+console.log(`---  BREAK WHEN NUMBER FOUND ---`);
+
+for (let i = 0; i < JonasArray.length; i++) {
+  if (typeof JonasArray[i] === 'break') break;
+  console.log(JonasArray[i], typeof JonasArray[i]);
+}
+
+*/
+
+
+//********************** 
+// 048 Looping Backwards and Loops in loops
+//**********************
+
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'Teacher',
+  ['Micheal', 'Peter', 'Jack', 'Rose'],
+
+];
+
+//looping backwards
+//5,4,3,2...0
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(jonas[i]);
+}
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+  console.log(`Starting Exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Rep ${rep}`);
+  }
+
+}
+  */
+
+//********************** 
+// 050 While Loop
+//**********************
+
+/*
+for (let rep = 0; rep < 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+
+
+console.log(`While loop result`);
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting weights repetition ${rep}`);
+  rep++;
+}
+*/
+
+
+//random number generator;
+let dice = Math.trunc(Math.random() * 6);
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6);
+  if (dice === 6) console.log(`Loop ends`);
 
 }
