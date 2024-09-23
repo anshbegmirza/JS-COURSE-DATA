8f4d2a101a616af5db492b443de0f845e640efd3
anshbegmirza <anshbegmirza@gmail.com>
2024-09-23 19:07:58 +0530

    Day 9 of JS

--- 6/6-Pig Game/script.js
+++ 6/6-Pig Game/script.js
@@ -21,16 +21,32 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, winningScore, playing;
 
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let winningScore = 20;
-const scores = [0, 0]; // final scores
-let currentScore = 0;
-let activePlayer = 0; //who is playing rightnow
-let playing = true;
+const init = function () {
+
+  scores = [0, 0]; // final scores
+  winningScore = 100;
+  currentScore = 0;
+  activePlayer = 0; //who is playing rightnow
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -88,3 +104,6 @@ btnHold.addEventListener('click', function () {
   }
 });
 
+
+//Reset the game
+btnNew.addEventListener('click', init);
