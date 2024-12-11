c5da53e18b69001229f0b7d44a77964eda140a44
anshbegmirza <anshbegmirza@gmail.com>
2024-12-11 19:12:11 +0530

    Learned how to fetch data from api, craeted simple modules a/c to MVC model architecture and refactored our forkify code

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -1,3 +1,15 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+
+import 'core-js/stable'; // pollyfill for the rest.
+import 'regenerator-runtime/runtime'; // pollyfill async await
+import recipeView from './views/recipeView.js';
+// console.log(icons);
+
+
+const { mark } = require("regenerator-runtime");
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -8,6 +20,40 @@ const timeout = function (s) {
   });
 };
 
+// this api is developed by Jonas himself
+
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+
+
+///////////////////////////////////////
+
+const controlRecipe = async function () {
+  try {
+
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1). loading a recipe from the api
+    await model.loadRecipe(id);
+    const recipe = model.state.recipe;
+
+    // 2). Rendering recipe
+    recipeView.render(model.state.recipe);
+    // const recipeView = new recipeView(model.state.recipe);
+  }
+  catch (err) {
+    alert(err);
+  }
+}
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
+// ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+const changeAndLoad = ['hashchange', 'load'];
+changeAndLoad.forEach(ev => window.addEventListener(ev, controlRecipe));
